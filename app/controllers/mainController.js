93fe9eea2d9ff80f05bb1e34e43063e9e6e0e671
Anton Chan <antoniuschan99@gmail.com>
2016-03-23 12:54:33 -0400

    fixed filter by genre

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -6,10 +6,14 @@
     shomiApp.controller(controllerId, ['$scope', '$state', 'mainViewModel', 'config', mainController]);
 
     function mainController($scope, $state, mainViewModel, config) {
+        var beginValue = 0;
 
         mainViewModel.init(config);
 
         $scope.mainModel = mainViewModel.model;
+
+        //Set Default Display
+        $scope.mainModel.selectBy = 'Item.Title';
 
         $scope.selectByTitle = function () {
             $scope.mainModel.selectBy = 'Item.Title';
@@ -31,6 +35,23 @@
             return number/60;
         }
 
+        $scope.next = function() {
+            if (beginValue + 20 < $scope.mainModel.categoryList.Data.length) {
+                beginValue = beginValue + 20;
+                $scope.mainModel.begin = beginValue;
+            }
+        }
+
+        $scope.previous = function() {
+            beginValue = beginValue - 20;
+            $scope.mainModel.begin = beginValue;
+        }
+
+        $scope.filterByGenre = function(genreName) {
+            $scope.mainModel.genre = genreName;
+        }
+
+        $scope.mainModel.selectedGenre = $scope.selectedGenre;
     };
 
 
